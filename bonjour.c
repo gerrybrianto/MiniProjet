0b4548790e884fbc97bd55c2d41599db0091aaa0
looleec <aniya93@hotmail.fr>
2015-10-14 11:55:43 +0200

    jai apporter ma pierre a lediffice

--- bonjour.c
+++ bonjour.c
@@ -3,7 +3,7 @@
 int main(){
 	char bj[15]="bonjour";
 	int rep;
-	printf("Veuillez entrer votre nombre de bonjour\n");
+	printf("Veuillez entrer votre modifienombreetmoiaussijemodifienombre de bonjour\n");
 	scanf("%d",&rep);
 	int i;
 	for(i=0;i<rep;i++)
